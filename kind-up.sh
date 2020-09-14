0ae29cbd44de4eade7e31d6847f961070e6e865f
Suchith J N <suchithjn22@gmail.com>
2020-09-14 09:15:46 +0530

    update script - add credits and some enhancements

--- kind-up.sh
+++ kind-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # A simple script to automate provisioning a kind cluster and a load balancer with metallb
+# This is useful for local development and testing Istio or other Kubernetes based projects
 #
 # Example usage: (Setup environment variables required) 
 # $ KIND_CLUSTER_NAME="istioio"
@@ -10,7 +11,7 @@
 # $ METALLB_END_IP=172.18.255.250 
 # ./kind-up.sh
 #
-# OR in a single line
+# OR in a single line (specified configuration is in Istio repository)
 # $ KIND_CLUSTER_NAME="istioio" KIND_CLUSTER_CONFIG="$GOPATH/src/istio.io/istio/prow/config/trustworthy-jwt.yaml" METALLB_SETUP_REQUIRED=1 METALLB_START_IP=172.18.255.1 METALLB_END_IP=172.18.255.250 ./kind-up.sh
 
 set -e
@@ -34,17 +35,21 @@ KUBECONFIG=${INSTALL_CONFIG:-"$HOME/.kube/config"}
 if [[ ! -z "${METALLB_SETUP_REQUIRED}" ]]; then
     if [[ -z "${METALLB_START_IP}" ]]; then
         echo "METALLB_START_IP should be specified"
+        exit 1
     fi
     if [[ -z "${METALLB_END_IP}" ]]; then
         echo "METALLB_END_ID should be specified"
+        exit 1
     fi
     
+    # This is taken from Metallb documentation page
     echo "Installing MetalLB for supporting LoadBalancer services"
     kubectl apply --kubeconfig="${KUBECONFIG}" -f "https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml"
     kubectl apply --kubeconfig="${KUBECONFIG}" -f "https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml"
     kubectl create --kubeconfig="${KUBECONFIG}" secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-      
-      echo 'apiVersion: v1
+    
+    # This is from Istio repository  
+    echo 'apiVersion: v1
 kind: ConfigMap
 metadata:
   namespace: metallb-system
