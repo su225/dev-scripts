9512442b797501d75c3badc0a8bbb9705fdb5e66
Suchith J N <suchithjn22@gmail.com>
2020-09-14 09:11:51 +0530

    script to bring up kind cluster and install metallb

---
+++ kind-up.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# A simple script to automate provisioning a kind cluster and a load balancer with metallb
+#
+# Example usage: (Setup environment variables required) 
+# $ KIND_CLUSTER_NAME="istioio"
+# $ KIND_CLUSTER_CONFIG="$GOPATH/src/istio.io/istio/prow/config/trustworthy-jwt.yaml" 
+# $ METALLB_SETUP_REQUIRED=1 
+# $ METALLB_START_IP=172.18.255.1 
+# $ METALLB_END_IP=172.18.255.250 
+# ./kind-up.sh
+#
+# OR in a single line
+# $ KIND_CLUSTER_NAME="istioio" KIND_CLUSTER_CONFIG="$GOPATH/src/istio.io/istio/prow/config/trustworthy-jwt.yaml" METALLB_SETUP_REQUIRED=1 METALLB_START_IP=172.18.255.1 METALLB_END_IP=172.18.255.250 ./kind-up.sh
+
+set -e
+
+if [[ -z "${KIND_CLUSTER_NAME}" ]]; then
+    echo "KIND_CLUSTER_NAME is not set"
+    exit 1
+fi
+
+if [[ -z "${KIND_CLUSTER_CONFIG}" ]]; then
+    echo "KIND_CLUSTER_CONFIG is not set"
+    exit 1
+fi
+
+kind create cluster \
+    --name="${KIND_CLUSTER_NAME}" \
+    --config="${KIND_CLUSTER_CONFIG}"
+    
+KUBECONFIG=${INSTALL_CONFIG:-"$HOME/.kube/config"}
+
+if [[ ! -z "${METALLB_SETUP_REQUIRED}" ]]; then
+    if [[ -z "${METALLB_START_IP}" ]]; then
+        echo "METALLB_START_IP should be specified"
+    fi
+    if [[ -z "${METALLB_END_IP}" ]]; then
+        echo "METALLB_END_ID should be specified"
+    fi
+    
+    echo "Installing MetalLB for supporting LoadBalancer services"
+    kubectl apply --kubeconfig="${KUBECONFIG}" -f "https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml"
+    kubectl apply --kubeconfig="${KUBECONFIG}" -f "https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml"
+    kubectl create --kubeconfig="${KUBECONFIG}" secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+      
+      echo 'apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - '"${METALLB_START_IP}-${METALLB_END_IP}" | kubectl apply --kubeconfig="${KUBECONFIG}" -f -
+fi
